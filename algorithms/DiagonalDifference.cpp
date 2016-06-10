d2451bcd45d666cfc87cb5233d028f0329a22e6c
Jonathan Chouraki <jonthan.chouraki@gmail.com>
2016-06-10 12:15:22 -0400

    More elegant solution

--- algorithms/DiagonalDifference.cpp
+++ algorithms/DiagonalDifference.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
-#include <vector>
 #include <cmath>
 
 int main(){
     int N;
     int primarySum = 0;
     int secondarySum = 0;
+    int value = 0;
     std::cin >> N;
-    int matrix[N][N];
     for(int i = 0; i < N; i++){
-       for(int j = 0; j < N; j++){
-           std::cin >> matrix[i][j];
-       }
-    }
-    for(int i = 0; i < N; i++){
-        primarySum += matrix[i][i];
-        secondarySum += matrix[i][N-1-i];
+        for(int j = 0; j < N; j++){
+            std::cin >> value;
+            if(i == j){
+                primarySum += value;
+            }
+            if(i == (N-1-j)){
+                secondarySum += value;
+            }
+        }
     }
     std::cout << std::abs(primarySum - secondarySum)  << std::endl;
 }
